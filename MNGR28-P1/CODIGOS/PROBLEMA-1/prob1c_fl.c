9dcfdce2cbaa1e015c86ec8e9f6306d35c046dc1
Arnau Mas <arnau.mas@outlook.com>
2018-03-04 18:13:40 +0100

    Problema 1 acabat

---
+++ MNGR28-P1/CODIGOS/PROBLEMA-1/prob1c_fl.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#define tol 1e-8
+
+int main(int argc, char* argv[]) {
+	/* La variable 'x' guardarà el valor introduït, i 'resultat' guardarà f(x) */
+  float x, resultat;
+
+	/* Comprovem que s'ha introduït un argument */
+  if((argc == 1) || (argc > 2)){
+    fprintf(stderr, "ERROR: Cal introduir un argument.\n");
+    return 1;
+  }
+
+	/* Comprovem que el paràmetre introduït dóna un valor vàlid */
+	if(!(x = (float)atof(argv[1]))) {
+		fprintf(stderr, "ERROR: Introduïu un nombre vàlid.\n");
+		return 1;
+	}
+
+	/* Si el nombre és prou proper a 0 retornem 1/2 */
+  if(fabs(x) < tol){
+    resultat = 0.5;
+  }
+  else {
+    resultat = (2 * sinf(x/2) * sinf(x/2)) / (x*x);
+  }
+
+  printf("f(%g) = %.8f\n", x, resultat);
+  return 0;
+}
