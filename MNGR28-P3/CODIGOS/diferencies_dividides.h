0ebca19bbb0eb6cfb66311cc8b892e57ed4281c8
Raquel García <raquelgarciabelles@gmail.com>
2018-04-22 20:57:06 +0200

    exercici 1 quasi acabat

--- MNGR28-P3/CODIGOS/diferencies_dividides.h
+++ MNGR28-P3/CODIGOS/diferencies_dividides.h
@@ -1,1 +1,8 @@
 void diferencies_dividides(double*, double*, double**, int n);
+double horner(int n, double *v, double *p, double x);
+
+
+double ff(double x); //f(x)
+void nodosequi(int n, double *v); //n número de nodes menys 1 (j=0, 1, ... ,n); es guarden els nodes en *v
+void nodoscheb(int n, double *v); //n número de nodes menys 1, es guarden els nodes en *v
+void puntosk(double *v);
--- MNGR28-P3/CODIGOS/prob12.c
+++ MNGR28-P3/CODIGOS/prob12.c
@@ -7,11 +7,20 @@ int main(int argc, char* argv[]) {
 	// 'nodes' és el fitxer de nom 'nom' que conté els nodes a partir dels quals es farà la interpolació
 	FILE* nodes; 
 	char* nom = argv[1];
+        
+        
+        FILE* fitxer; //guardarem els punts i el resultat d'avaluarlos amb el polinomi
+        FILE* fitxer1; //guardarem els 180 punts i el resultat d'avaluarlos amb la funcio
+        fitxer=fopen("kavaluats.txt", "w"); 
+        fitxer1=fopen("k.txt", "w");
 
 	double* x;
 	double* f;
 	double** difs;
-
+        double *coef; //vector on guardarem els coeficients del polinomi de lagrange. 
+        double *k, *pk, *fk; //k punts on avaluarem, pk resultat d'avaluar els punts amb el p. de lagrange, fk resultat d'avaluar els punts a la funció
+        double max, kmax; //max serà la discrepància màxima entre el resultat d'avaluar els punts amb el polinomi de lagrange i amb la funció, kmax serà el punt on es produeix.
+        
 	// Comprovem que el fitxer es pot obrir
 	if((nodes = fopen(nom, "r")) == NULL) {
 		fprintf(stderr, "ERROR: El fitxer introduït no existeix o no es pot obrir.\n");	
@@ -45,15 +54,43 @@ int main(int argc, char* argv[]) {
 
 	// Llegim els nodes
 	for(i = 0; i < n; i++) {
-		if((fscanf(nodes,"%lf, %lf", &x[i], &f[i])) != 2) {
+		if((fscanf(nodes,"%lf\t%lf", &x[i], &f[i])) != 2) {
 			fprintf(stderr, "ERROR: No s'han pogut llegir correctament les dades.\n");
 		}
 	}
 
 	diferencies_dividides(x, f, difs, n);
+        coef= malloc(n*sizeof(double));
+        
 	for(i = 0; i < n; i++) {
-		printf("difs[%d][%d] = %lf \n", i, i, difs[i][i]);
+		//printf("difs[%d][%d] = %lf \n", i, i, difs[i][i]);
+                //printf("x[%d]=%lf\n", i, x[i]);
+                coef[i]=difs[i][i];
 	}
+	
+        k=malloc(181*sizeof(double)); //punts on avaluarem
+        pk=malloc(181*sizeof(double)); //valors del polinomi al punt avaluat
+        puntosk(k); //plenem k
+        fk=malloc(181*sizeof(double)); //valors de la funció al punt avaluat
+        
+        for(i=0;i<=180;i++){ //polinomi de lagrange
+            pk[i]=horner(n, coef, x, k[i]);
+            fprintf(fitxer, "%lf\t%lf\n",k[i], pk[i]);
+        }
+            
+        for(i=0;i<=180;i++){ //funció
+            fk[i]=ff(k[i]);
+            fprintf(fitxer1, "%lf\t%lf\n",k[i], fk[i]);
+        }
+        max=0;
+        for(i=0;i<=180;i++){
+            if(fabs(pk[i]-fk[i])>max){
+                max=fabs(pk[i]-fk[i]);
+                kmax=k[i];
+            }
+        }
+        printf("La discrepància màxima entre la funció i el polinomi de lagrange és %lf i es produeix al punt $x_k=%lf\n$", max, kmax);
+        
 	return 0;
 }
 
