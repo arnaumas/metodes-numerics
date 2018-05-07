078a9ee05ee24101b7f61015e0b16864c6029600
Raquel García <raquelgarciabelles@gmail.com>
2018-05-07 13:27:41 +0200

    arreglat el 5

--- MNGR28-P3/CODIGOS/PROBLEMA-5/prob5.c
+++ MNGR28-P3/CODIGOS/PROBLEMA-5/prob5.c
@@ -7,7 +7,7 @@ int main() {
     double s, *fv; //s guardara el resultat de simpson, fv guardara els valors d'avaluar la funció en els punts que divideixen l'interval.
     int i; //comptador
     
-    for(i=2;i<=8;i=i+2){ //tot i que amb n=2 és suficient, calculem la integral per a diversos valors de n per a comparar.
+    for(i=2;i<=20;i=i+2){ //tot i que amb n=2 és suficient, calculem la integral per a diversos valors de n per a comparar.
         fv=malloc((i+1)*sizeof(double));
         f3(i, 1, 2,fv);
         s=simpson(i, 1, 2, fv); //regla composta de simpson
